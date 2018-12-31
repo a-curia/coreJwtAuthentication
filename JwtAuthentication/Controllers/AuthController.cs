ded22fd85569dd309a92f08a43b714b07a00e4a1
Gheorghe-Adrian Curia <gheorgheadrian.curia@ihsmarkit.com>
2018-12-31 20:27:08 +0200

    register and login methods

---
+++ JwtAuthentication/Controllers/AuthController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using JwtAuthentication.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JwtAuthentication.Controllers
+{
+    public class AuthController : Controller
+    {
+
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(UserManager<IdentityUser> userManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        [Route("register")]
+        [HttpPost]
+        public async Task<ActionResult> RegisterUser([FromBody] RegisterViewModel model)
+        {
+            var user = new IdentityUser
+            {
+                Email = model.Email,
+                UserName = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, "Customer");
+            }
+
+            return Ok(new { Username = user.UserName });
+        }
+
+        [Route("login")]
+        [HttpPost]
+        public async Task<ActionResult> LoginUser([FromBody] LoginViewModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.Username);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var claim = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, user.UserName)
+                };
+
+                var signinKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:SigninKey"]));
+
+                var expiresInMinutes = Convert.ToInt32(_configuration["Jwt:ExpiryInMinutes"]);
+
+                var token = new JwtSecurityToken(
+                        issuer: _configuration["Jwt:Site"],
+                        audience: _configuration["Jwt:Site"],
+                        expires: DateTime.UtcNow.AddMinutes(expiresInMinutes),
+                        signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
+                    );
+
+                return Ok(
+                        new
+                        {
+                            token = new JwtSecurityTokenHandler().WriteToken(token),
+                            expiration = token.ValidTo
+                        }
+                    );
+
+            }
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, "Customer");
+            }
+
+            return Ok(new { Username = user.UserName });
+        }
+
+    }
+}
